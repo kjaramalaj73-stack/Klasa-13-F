118d5fa93e9663a67f9e2b42bb541f63f2b1ac33
Kjara <kjaramalaj73@gmail.com>
2025-10-14 17:05:53 +0200

    Komiti im i pare

--- convert7.php
+++ convert7.php
@@ -1,22 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Marrim të dhënat nga forma
-    $euro = $_POST["euro"];
-    $kursi = $_POST["kursi"];
 
-    // Siguri bazë (pastrim të dhënash)
-    $euro = htmlspecialchars($euro);
+    $shuma = $_POST["shuma"];
+    $monedha_origjinale = $_POST["monedha_origjinale"];
+    $monedha_destinacion = $_POST["monedha_destinacion"];
+    $kursi = $_POST["kursi"]; 
+
+    $shuma = htmlspecialchars($shuma);
+    $monedha_origjinale = htmlspecialchars($monedha_origjinale);
+    $monedha_destinacion = htmlspecialchars($monedha_destinacion);
     $kursi = htmlspecialchars($kursi);
 
-    // Llogaritja
-    $lek = $euro * $kursi;
+    
+    $rezultati = $shuma * $kursi;
+    $rezultati = round($rezultati, 2);
 
     echo "<h1>Rezultati i Këmbimit</h1>";
-    echo "<p>Shuma në EURO: <b>$euro €</b></p>";
+    echo "<p>Shuma origjinale: <b>$shuma $monedha_origjinale</b></p>";
     echo "<p>Kursi i këmbimit: <b>$kursi</b></p>";
-    echo "<p><b>Shuma e konvertuar në LEK (ALL): $lek Lekë</b></p>";
+    echo "<p>Shuma e konvertuar në <b>$monedha_destinacion</b>: <b>$rezultati $monedha_destinacion</b></p>";
 
     echo "<br><a href='index7.html'>Kthehu mbrapa</a>";
+
 } else {
     echo "Nuk keni futur të dhëna!";
 }
